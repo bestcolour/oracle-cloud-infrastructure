9a38c04eb998550063834fc5a61c300c969b75b1
bestcolour <49665281+bestcolour@users.noreply.github.com>
2026-05-28 00:24:21 +0800

    Added headscale instance

--- 01-workloads/main-compute.tf
+++ 01-workloads/main-compute.tf
@@ -257,6 +257,109 @@ resource "oci_core_public_ip" "main_reserved_public_ip" {
 }
 
 
+# ===== Headscale Instance =========
+# --- computing instance related Variables ---
+
+variable "headscale_vm_name" {
+  type        = string 
+  description = "The name of the compute instance provisioned for the role of headscale"
+}
+variable "headscale_vm_memory" {
+  type        = number 
+  description = "The memory of the compute instance provisioned for the role of headscale"
+}
+variable "headscale_vm_ocpus" {
+  type        = number 
+  description = "The ocpus of the compute instance provisioned for the role of headscale"
+}
+variable "headscale_vm_source_id" {
+  type        = string 
+  description = "The image id of the os image you want your vm instance to use. Find your this value here https://docs.oracle.com/en-us/iaas/images/ > choose the image you want to use > copy the link based on your region"
+}
+variable "headscale_vm_assign_public_ip" {
+  type        = bool 
+  description = "Whether the VNIC should be assigned a public IP address. Defaults to whether the subnet is public or private. If not set and the VNIC is being created in a private subnet (that is, where prohibitPublicIpOnVnic = true in the Subnet), then no public IP address is assigned. If not set and the subnet is public (prohibitPublicIpOnVnic = false), then a public IP address is assigned. If set to true and prohibitPublicIpOnVnic = true, an error is returned"
+}
+variable "headscale_vm_hostname_label" {
+  type        = string 
+  description = "The hostname for the VNIC's primary private IP. Used for DNS. The value is the hostname portion of the primary private IP's fully qualified domain name (FQDN) (for example, bminstance1 in FQDN bminstance1.subnet123.vcn1.oraclevcn.com). Must be unique across all VNICs in the subnet and comply with RFC 952 and RFC 1123. The value appears in the Vnic object and also the PrivateIp object returned by ListPrivateIps and GetPrivateIp."
+}
+
+
+
+# --- computing core instance ---
+resource "oci_core_instance" "headscale_vm" {
+    #Required
+    availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+    compartment_id      = oci_identity_compartment.data_arch_compartment.id
+    display_name        = var.headscale_vm_name
+    shape               = var.free_forever_compute_shape
+
+    create_vnic_details {
+
+        #Optional
+        # assign_ipv6ip = var.instance_create_vnic_details_assign_ipv6ip
+        # assign_private_dns_record = var.instance_create_vnic_details_assign_private_dns_record
+        assign_public_ip = var.headscale_vm_assign_public_ip
+        # defined_tags = {"Operations.CostCenter"= "42"}
+        # display_name = var.instance_create_vnic_details_display_name
+        # freeform_tags = {"Department"= "Finance"}
+        hostname_label = var.headscale_vm_hostname_label
+        nsg_ids        = [oci_core_network_security_group.private_network_security_group.id]
+        # security_attributes = var.headscale_vm_security_attributes # not using since this is outside of free forever tier
+
+        subnet_id = oci_core_subnet.main_vcn_private_subnet.id
+    }
+
+
+    # security_attributes = var.headscale_vm_security_attributes # not using since this is outside of free forever tier
+    # shape = var.instance_shape
+    shape_config {
+      memory_in_gbs = var.headscale_vm_memory
+      ocpus         = var.headscale_vm_ocpus
+    }
+    source_details {
+        #Required
+        source_id   = var.headscale_vm_source_id
+        source_type = "image"
+
+        # #Optional
+        # boot_volume_size_in_gbs = var.instance_source_details_boot_volume_size_in_gbs
+        # boot_volume_vpus_per_gb = var.instance_source_details_boot_volume_vpus_per_gb
+        # instance_source_image_filter_details {
+        #      #Required
+        #      compartment_id = var.compartment_id
+
+        #      #Optional
+        #      defined_tags_filter = var.instance_source_details_instance_source_image_filter_details_defined_tags_filter
+        #      operating_system = var.instance_source_details_instance_source_image_filter_details_operating_system
+        #      operating_system_version = var.instance_source_details_instance_source_image_filter_details_operating_system_version
+        # }
+        # kms_key_id = oci_kms_key.test_key.id
+    }
+    metadata = {
+      ssh_authorized_keys = tls_private_key.headscale_vm_ssh_key.public_key_openssh
+      # user_data = base64encode(
+      #   templatefile("${path.module}/main-compute-setup-headscale.sh.tpl",
+      #   {
+      #     your_tcp_ports              = join(" ", var.your_tcp_ports)
+      #     your_duckdns_token          = var.your_duckdns_token
+      #     your_duckdns_domainname     = var.your_duckdns_domainname
+      #     your_base_domain            = var.your_base_domain
+      #     headscale_private_ip_n_port = var.headscale_private_ip_n_port
+      #     projects_private_ip_n_port  = var.projects_private_ip_n_port        
+      #   }
+      #   )
+      # )
+    }
+    preserve_boot_volume = false
+      
+
+  depends_on = [ tls_private_key.headscale_vm_ssh_key, oci_core_network_security_group.private_network_security_group ]
+}
+
+
+
 # ===== Network Security Groups (NSG) =========
 variable "reverse_proxy_NSG_display_name" {
   type = string
