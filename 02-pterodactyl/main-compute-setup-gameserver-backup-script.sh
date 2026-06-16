77d02155739b3db035518fbccbb757f95eb939ae
bestcolour <49665281+bestcolour@users.noreply.github.com>
2026-06-17 01:54:28 +0800

    Rclone successfully working
    Rclone successfully working:
    
    1) main-compute-setup-gameserver-backup-script.sh: added comments about the rclone remote name (created during the config setup process) needing to share the same name as the value "pterodactyl_cloud_backup" written in this shell script.
    
    2) main-compute-setup-gameserver-playbook.yml:
    Added the permission command line to the playbook so that docker rclone can write the config of the remote inside of that folder

--- 02-pterodactyl/main-compute-setup-gameserver-backup-script.sh
+++ 02-pterodactyl/main-compute-setup-gameserver-backup-script.sh
@@ -19,20 +19,28 @@ sudo docker compose exec -T database mariadb-dump -u pterodactyl -p"$DB_PASSWORD
 # We mount the host root '/' to '/hostfs' in read-only mode so rclone can access all necessary paths.
 echo "Syncing files to cloud storage..."
 
+
+# =============== Backup Logic ===============
+# The name 'pterodactyl_cloud_backup' should be the same as the name of the remote you configured when you ran:
+# sudo docker run -it --rm --volume /etc/rclone:/config/rclone --user $(id -u):$(id -g) rclone/rclone config
+
+# This docker run backs up the pterodactyl panel data
 sudo docker run --rm \
   --volume /etc/rclone:/config/rclone \
   --volume /:/hostfs:ro \
-  rclone/rclone sync /hostfs/srv/pterodactyl cloud_backup:/pterodactyl_backups/panel_data --progress
+  rclone/rclone sync /hostfs/srv/pterodactyl pterodactyl_cloud_backup:/pterodactyl_backups/panel_data --progress
 
+# This docker run backs up the pterodactyl wings data
 sudo docker run --rm \
   --volume /etc/rclone:/config/rclone \
   --volume /:/hostfs:ro \
-  rclone/rclone sync /hostfs/etc/pterodactyl cloud_backup:/pterodactyl_backups/wings_config --progress
+  rclone/rclone sync /hostfs/etc/pterodactyl pterodactyl_cloud_backup:/pterodactyl_backups/wings_config --progress
 
+# This docker run backs up the pterodactyl wings game server data
 # /var/lib/pterodactyl contains the Wings game server volumes and local backup archives
 sudo docker run --rm \
   --volume /etc/rclone:/config/rclone \
   --volume /:/hostfs:ro \
-  rclone/rclone sync /hostfs/var/lib/pterodactyl cloud_backup:/pterodactyl_backups/game_servers --progress
+  rclone/rclone sync /hostfs/var/lib/pterodactyl pterodactyl_cloud_backup:/pterodactyl_backups/game_servers --progress
 
 echo "Backup sequence completed successfully."
