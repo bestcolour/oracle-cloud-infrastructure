eeb1a28b9e152116b44c6a6ed2a318cfc144d6af
bestcolour <49665281+bestcolour@users.noreply.github.com>
2026-06-16 16:24:16 +0800

    Change project compartment names
    Change project compartment names:
    
    1) changed the compartment holding the projects' cloud resource to their respective project number and names

--- 02-pterodactyl/main.tf
+++ 02-pterodactyl/main.tf
@@ -58,7 +58,7 @@ resource "oci_identity_compartment" "data_arch_compartment" {
     # Required
     compartment_id = var.second_root_compartment_ocid
     description = "This compartment holds the cloud resources that are provisioned for the main cloud architecture"
-    name = "data-arch-compartment"
+    name = "02-pterodactyl"
     enable_delete = true
 }
 
